1720f55f30d79ee80688255e475bc97591ab8b0e
mamadaa <marlouamada28@gmail.com>
2021-12-12 22:27:48 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,19 @@ function App() {
             bulk of the card's content.
           </p>
           <a href="#" class="btn btn-primary">
-            Go somewhere
+            Go somewhere everywhere
+          </a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere everywhere sadasd
           </a>
         </div>
       </div>
