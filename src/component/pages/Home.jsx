459ced5360ea671011b14c8a9b56c3e9467972be
mamadaa <marlouamada28@gmail.com>
2021-12-13 22:49:32 +0800

    12/13/2021 (10:49 pm) update

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -3,24 +3,8 @@ import "./css/home.css";
 
 export default function Home() {
   return (
-    <div className="about-box ">
-      <div className="about-first position-relative ">
-        <div className="header position-absolute mt-sm-4 start-50 translate-middle-x ">
-          <div class="gdsc mt-3">Google Developer Student Clubs</div>
-          <div class="usa">University of San Agustin</div>
-          <p class="mt-md-5 mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel mauris
-            mattis tempor elementum eget tortor mi nec auctor. Vulputate enim
-            tortor augue et est vulputate etiam ut magna. Et donec ultricies
-            semper maecenas risus sed. Commodo vitae faucibus gravida nulla.
-            Phasellus mauris duis turpis dolor aliquam porttitor. In tincidunt
-            et nulla odio nec consectetur. Diam, tempus erat hendrerit dictumst
-            sed a tincidunt. Aliquet gravida condimentum donec ipsum.
-          </p>
-        </div>
-      </div>
-
-      <div className="about-mission ">
+    <div className="Home-box ">
+      <div className="Home ">
         <div class="mission-flex d-flex justify-content-center  ">
           <div class="container text-center mt-md-5">
             <h1 class="ms-sm-1">Our mission</h1>
@@ -37,24 +21,6 @@ export default function Home() {
           </div>
 
           <div class="container con1"></div>
-        </div>
-      </div>
-      <div className="about-mission ">
-        <div class="mission-flex d-flex justify-content-center  ">
-          <div class="container con1"></div>
-          <div class="container text-center mt-md-5">
-            <h1 class="ms-sm-1">Our vission</h1>
-            <p class="ms-sm-1 mt-sm-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-              mauris mattis tempor elementum eget tortor mi nec auctor.
-              Vulputate enim tortor augue et est vulputate etiam ut magna. Et
-              donec ultricies semper maecenas risus sed. Commodo vitae faucibus
-              gravida nulla. Phasellus mauris duis turpis dolor aliquam
-              porttitor. In tincidunt et nulla odio nec consectetur. Diam,
-              tempus erat hendrerit dictumst sed a tincidunt. Aliquet gravida
-              condimentum donec ipsum.
-            </p>
-          </div>
         </div>
       </div>
     </div>
--- src/component/pages/Signup.jsx
+++ src/component/pages/Signup.jsx
@@ -1,14 +1,107 @@
 import React from "react";
+import "./css/signup.css";
+import exit from "../img/exit.svg";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <div>
-      <span>
-        <Link to="/" class="nav-link">
-          Log in
-        </Link>
-      </span>
+    <div className="Singup-box">
+      <div className="Signup ">
+        <div className="Signup-container ">
+          <div class="wrapper-flex d-flex justify-content-center">
+            <div class="container">
+              <ul class="navbar-nav  ">
+                <li class="me-4">
+                  <Link to="/" class="exit nav-link float-start">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src={exit} alt="exit" />
+                  </Link>
+                </li>
+              </ul>
+              <h1 class=" fw-bolder text-center mt-2">Sign up to Blank</h1>
+
+              <form>
+                <div class="mb-3 ">
+                  <label for="exampleInputEmail1" class="form-label fw-bolder">
+                    Name
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control rounded-pill"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label
+                    for="exampleInputPassword1"
+                    class="form-label fw-bolder"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control rounded-pill"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div class="mb-3">
+                  <label
+                    for="exampleInputPassword1"
+                    class="form-label fw-bolder"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control rounded-pill"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div class="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label class="form-check-label" for="exampleCheck1">
+                    Terms and condition
+                  </label>
+                </div>
+
+                <div class="sign-in d-grid gap-2 mt-5">
+                  <button class="btn rounded-pill" type="button">
+                    Create account
+                  </button>
+                  <button
+                    class="btn btn2  border border-dark rounded-pill"
+                    type="button"
+                  >
+                    Sign up with Google
+                  </button>
+                </div>
+              </form>
+
+              <span class="sign-in ms-5">
+                Already have an account?
+                <a
+                  class="btn btn-light m-4 rounded-pill "
+                  href="#"
+                  role="button"
+                >
+                  Sign in
+                </a>
+              </span>
+            </div>
+
+            <div class="container con1 "></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
+
+{
+  /* */
 }
