2f956f1f8b9211137390459898ab422390439e38
mamadaa <marlouamada28@gmail.com>
2021-12-13 18:43:37 +0800

    12/13/2021 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import "./App.css";
-import LandingNavbar from "./component/inc/LandingNavbar";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingNavbar from "./component/inc/Navbar.jsx";
+import About from "./component/pages/About.jsx";
+import Home from "./component/pages/Home.jsx";
+import Testimonials from "./component/pages/Testimonials";
+import Contact from "./component/pages/Contact";
+import Signup from "./component/pages/Signup.jsx";
+import Login from "./component/pages/Login";
 
 function App() {
   return (
     <Router>
-      <div className="navs">
-        <LandingNavbar />
-      </div>
+      <Switch>
+        <Route exact path="/Signup" component={Signup} />
+        <Route exact path="/Login" component={Login} />
+        <div className="navs">
+          <LandingNavbar />
+          <Route exact path="/" component={Home}></Route>
+        </div>
+      </Switch>
     </Router>
   );
 }
---
+++ src/component/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+  return (
+    <div>
+      <span>
+        <Link to="/" class="nav-link">
+          Log in
+        </Link>
+      </span>
+    </div>
+  );
+}
