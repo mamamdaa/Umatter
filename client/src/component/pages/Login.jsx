62902b9936f201224ba304275c37da12626a56ce
mamadaa <marlouamada28@gmail.com>
2021-12-15 01:08:33 +0800

    update

--- client/src/component/pages/Login.jsx
+++ client/src/component/pages/Login.jsx
@@ -1,37 +1,45 @@
 import React from "react";
 import "./css/login.css";
 import exit from "../img/exit.svg";
+import background from "./css/background.svg";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <div className="Login-box">
-      <div className="Login ">
-        <div className="Login-container ">
-          <div class="wrapper-flex d-flex justify-content-center">
-            <div class="container">
+      <div className="Login">
+        <div class="Login-flex d-flex justify-content-center p-4 ">
+          <div className="container">
+            {" "}
+            <img class="ms-5" src={background} alt="background" />
+          </div>
+
+          <div className="Form-flex ">
+            <div className="container border">
               <ul class="navbar-nav  ">
-                <li class="me-4">
+                <li>
                   <Link to="/" class="exit nav-link float-start">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <img src={exit} alt="exit" />
                   </Link>
                 </li>
               </ul>
-              <h1 class=" fw-bolder text-center mt-2">Sign up to Blank</h1>
-
+              <h1 class=" fw-bolder text-center">Sign up</h1>
+              <div class="sign-in d-grid gap-2 mt-4">
+                <button class="btn btn2  border  " type="button">
+                  Sign in with Google
+                </button>
+                <p class="text-center mt-3 mb-3 fw-bold">or</p>
+              </div>
               <form>
                 <div class="mb-3">
-                  <label
-                    for="exampleInputPassword1"
-                    class="form-label fw-bolder"
-                  >
+                  <label for="exampleInputEmail1" class="form-label fw-bolder">
                     Email
                   </label>
                   <input
                     type="Email"
-                    class="form-control rounded-pill"
-                    id="exampleInputPassword1"
+                    class="form-control fw-bold border border-dark"
+                    id="exampleInputEmail1"
                   />
                 </div>
                 <div class="mb-3">
@@ -43,52 +51,34 @@ export default function Login() {
                   </label>
                   <input
                     type="password"
-                    class="form-control rounded-pill"
+                    class="form-control fw-bold border border-dark"
                     id="exampleInputPassword1"
                   />
                 </div>
-                <div class="mb-3 form-check">
-                  <input
-                    type="checkbox"
-                    class="form-check-input"
-                    id="exampleCheck1"
-                  />
-                  <label class="form-check-label" for="exampleCheck1">
-                    Terms and condition
-                  </label>
+                <div class="forgot mb-3 text-center">
+                  <a href="#" class="link-dark">
+                    Forgot Password?
+                  </a>
                 </div>
 
                 <div class="sign-in d-grid gap-2 mt-5">
-                  <button class="btn rounded-pill" type="button">
-                    Sign in
-                  </button>
-                  <button
-                    class="btn btn2  border border-dark rounded-pill"
-                    type="button"
-                  >
-                    Sign in with Google
+                  <button class="btn fw-bold border border-dark" type="button">
+                    Create account
                   </button>
                 </div>
               </form>
-
               <div className="sign-up d-flex justify-content-center">
                 {" "}
                 <span class="sign-up-container ms-sm-5">
                   <a>Not a member?</a>
                   <Link to="/Signup">
-                    <a
-                      class="btn btn-light m-4 rounded-pill "
-                      href="#"
-                      role="button"
-                    >
+                    <a class="btn btn-light m-4 " role="button">
                       Sign up
                     </a>
                   </Link>
                 </span>
               </div>
             </div>
-
-            <div class="container con2 ">adsa</div>
           </div>
         </div>
       </div>
--- client/src/component/pages/Signup.jsx
+++ client/src/component/pages/Signup.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import "./css/signup.css";
 import exit from "../img/exit.svg";
+import background from "./css/background.svg";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
     <div className="Singup-box">
-      <div className="Signup ">
-        <div className="Signup-container ">
-          <div class="wrapper-flex d-flex justify-content-center">
-            <div class="container">
+      <div className="Signup">
+        <div class="Signup-flex d-flex justify-content-center p-4 ">
+          <div className="container">
+            {" "}
+            <img class="ms-5" src={background} alt="background" />
+          </div>
+
+          <div className="Form-flex ">
+            <div className="container border">
               <ul class="navbar-nav  ">
-                <li class="me-4">
+                <li>
                   <Link to="/" class="exit nav-link float-start">
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <img src={exit} alt="exit" />
                   </Link>
                 </li>
               </ul>
-              <h1 class=" fw-bolder text-center mt-2">Sign up to Blank</h1>
-
+              <h1 class=" fw-bolder text-center">Sign up</h1>
+              <div class="sign-up d-grid gap-2 mt-4">
+                <button class="btn btn2  border  " type="button">
+                  Sign up with Google
+                </button>
+                <p class="text-center mt-3 mb-3 fw-bold">or</p>
+              </div>
               <form>
                 <div class="mb-3 ">
                   <label for="exampleInputEmail1" class="form-label fw-bolder">
@@ -27,22 +38,19 @@ export default function Signup() {
                   </label>
                   <input
                     type="email"
-                    class="form-control rounded-pill"
+                    class="form-control fw-bold border border-dark"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                   />
                 </div>
                 <div class="mb-3">
-                  <label
-                    for="exampleInputPassword1"
-                    class="form-label fw-bolder"
-                  >
+                  <label for="exampleInputEmail1" class="form-label fw-bolder">
                     Email
                   </label>
                   <input
                     type="Email"
-                    class="form-control rounded-pill"
-                    id="exampleInputPassword1"
+                    class="form-control fw-bold border border-dark"
+                    id="exampleInputEmail1"
                   />
                 </div>
                 <div class="mb-3">
@@ -54,7 +62,7 @@ export default function Signup() {
                   </label>
                   <input
                     type="password"
-                    class="form-control rounded-pill"
+                    class="form-control fw-bold border border-dark"
                     id="exampleInputPassword1"
                   />
                 </div>
@@ -70,36 +78,23 @@ export default function Signup() {
                 </div>
 
                 <div class="sign-up d-grid gap-2 mt-5">
-                  <button class="btn rounded-pill" type="button">
+                  <button class="btn fw-bold border border-dark" type="button">
                     Create account
-                  </button>
-                  <button
-                    class="btn btn2  border border-dark rounded-pill"
-                    type="button"
-                  >
-                    Sign up with Google
                   </button>
                 </div>
               </form>
-
               <div className="sign-in d-flex justify-content-center">
                 {" "}
                 <span class="sign-in-container ms-sm-5">
                   <a>Already have an account?</a>
                   <Link to="/Login">
-                    <a
-                      class="btn btn-light m-4 rounded-pill "
-                      href="#"
-                      role="button"
-                    >
+                    <a class="btn btn-light m-4 " href="#" role="button">
                       Sign in
                     </a>
                   </Link>
                 </span>
               </div>
             </div>
-
-            <div class="container con1 "></div>
           </div>
         </div>
       </div>
@@ -108,5 +103,5 @@ export default function Signup() {
 }
 
 {
-  /* */
+  /*    */
 }
