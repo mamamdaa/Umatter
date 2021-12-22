3484b1dfa6ce20cf1af738222d413121eac59565
cdgmx <iamcdgmx@gmail.com>
2021-12-22 18:31:40 +0800

    Beutify

--- graphql/schemas/index.js
+++ graphql/schemas/index.js
@@ -1,36 +1,38 @@
-const UserQuery = require('./queries/UserQuery');
-const ChannelQuery = require('./queries/ChannelQuery');
-const UserMutation = require('./mutations/UserMutation');
-const ChannelMutations = require('./mutations/ChannelMutations');
-const MessageMutations = require('./mutations/MessageMutations');
-const UserSubscription = require('./subscriptions/UserSubscription');
-const ChannelSubscription = require('./subscriptions/ChannelSubscription');
-const graphql = require('graphql');
-const {
-  GraphQLObjectType,
-  GraphQLList,
-  GraphQLInt
-} = graphql
+const UserQuery = require("./queries/UserQuery");
+const ChannelQuery = require("./queries/ChannelQuery");
+const UserMutation = require("./mutations/UserMutation");
+const ChannelMutations = require("./mutations/ChannelMutations");
+const MessageMutations = require("./mutations/MessageMutations");
+const FacilitatorMutations = require("./mutations/FacilitatorMutations");
+const UserSubscription = require("./subscriptions/UserSubscription");
+const ChannelSubscription = require("./subscriptions/ChannelSubscription");
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLList, GraphQLInt } = graphql;
 
 const Query = new GraphQLObjectType({
-    name: 'Query',
-    fields: {...UserQuery,...ChannelQuery}
-    
-})
+  name: "Query",
+  fields: { ...UserQuery, ...ChannelQuery },
+});
 
 const Mutation = new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {...UserMutation,...ChannelMutations,...MessageMutations}
+  name: "Mutation",
+  fields: {
+    ...UserMutation,
+    ...ChannelMutations,
+    ...MessageMutations,
+    ...FacilitatorMutations,
+  },
 });
 
 const Subscription = new GraphQLObjectType({
-    name: 'Subscription',
-    fields: {...UserSubscription,...ChannelSubscription}
+  name: "Subscription",
+  fields: { ...UserSubscription, ...ChannelSubscription },
 });
-// console.log('combine', {
-//   ...MessageMutations,
-//   ...ChannelMutations,
-// });
-const schema = new graphql.GraphQLSchema({query:Query,mutation:Mutation,subscription:Subscription});
+
+const schema = new graphql.GraphQLSchema({
+  query: Query,
+  mutation: Mutation,
+  subscription: Subscription,
+});
 
 module.exports = schema;
