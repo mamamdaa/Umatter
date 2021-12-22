d2bf03fae2f681bb63a20c62f97e63728763c90e
cdgmx <iamcdgmx@gmail.com>
2021-12-22 15:47:57 +0800

    Dynamic ENV
    removed scrolltotop variable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,10 +11,10 @@ import Login from "./component/pages/Login";
 
 import User from "./component/pages/User.jsx";
 import store from "./redux/store";
-import {Provider} from "react-redux";
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-import { split } from 'apollo-link';
+import { Provider } from "react-redux";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import { split } from "apollo-link";
 import {
   ApolloClient,
   InMemoryCache,
@@ -23,15 +23,19 @@ import {
   from,
   gql,
 } from "@apollo/client";
-import ScrollToTop from "./component/inc/ScrollTop.jsx";
 import { onError } from "@apollo/client/link/error";
 
-
-// console.log("test",process.env.REACT_APP_API_URL);
-// const baseLink = "localhost:5000/graphql"
-const baseLink = process.env.REACT_APP_API_URL
-console.log("baseLink",baseLink);
-
+const baseLink = process.env.REACT_APP_API_URL;
+const environment = process.env.NODE_ENV;
+console.log("baseLink", baseLink);
+const websocketURI =
+  environment === "development"
+    ? "ws://localhost:5000/graphql"
+    : "wss://" + baseLink + "/graphql"
+const httpURI =
+  environment === "development"
+    ? "http://localhost:5000/graphql"
+    : "https://" + baseLink + "/graphql";
 const errorLink = onError(({ graphqlErrors, networkError }) => {
   if (graphqlErrors) {
     graphqlErrors.map(({ message, location, path }) => {
@@ -44,35 +48,31 @@ const errorLink = onError(({ graphqlErrors, networkError }) => {
 });
 
 const wsLink = new WebSocketLink({
-  uri: `wss://`+baseLink+`/graphql`,
+  uri: websocketURI,
   options: {
-    reconnect: true
-  }
+    reconnect: true,
+  },
 });
-
 
 const link = split(
   // split based on operation type
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
 
-  new HttpLink({ uri: "http://"+ baseLink+`/graphql`}),
-  errorLink,
+  new HttpLink({ uri: httpURI }),
+  errorLink
 );
 
 // const link = from([
 //   errorLink,
 //   new HttpLink({ uri: "http://localhost:5000/graphql" }),
 // ]);
-
-
-
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
