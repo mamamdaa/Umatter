2bd3e14fcf3813953a99bce91dac997d3e45b115
cdgmx <iamcdgmx@gmail.com>
2021-12-31 04:01:28 +0800

    commits

--- client/src/graphql/Mutations.js
+++ client/src/graphql/Mutations.js
@@ -1,21 +1,51 @@
 import { gql } from "@apollo/client";
 
-export const REGISTER =  gql`
-    mutation addUser($last_name:String!,$first_name:String!,$email:String!, $password:String!){
-        addUser(last_name:$last_name,first_name:$first_name,email:$email, password:$password) {
-        _id
-        email
-        }
+export const REGISTER = gql`
+  mutation addUser(
+    $last_name: String!
+    $first_name: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      last_name: $last_name
+      first_name: $first_name
+      email: $email
+      password: $password
+    ) {
+      _id
+      email
     }
+  }
 `;
 
 export const SEND_MESSAGE = gql`
-    mutation SendMessage($text: String!, $channel: String!, $sender: String!) {
-        sendMessage(text: $text, channel: $channel, sender: $sender) {
-        _id
-        text
-        sender
-        sender_name
-        }
+  mutation SendMessage($text: String!, $channel: String!, $sender: String!) {
+    sendMessage(text: $text, channel: $channel, sender: $sender) {
+      _id
+      text
+      sender
+      sender_name
     }
+  }
+`;
+
+export const ENTER_QUEUE = gql`
+  mutation Mutation($id: String) {
+    enterQueue(_id: $id) {
+      is_in_queue
+      first_name
+      email
+    }
+  }
+`;
+
+export const LEAVE_QUEUE = gql`
+  mutation Mutation($id: String) {
+    leaveQueue(_id: $id) {
+      first_name
+      email
+      is_in_queue
+    }
+  }
 `;
